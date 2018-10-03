34dadd175e60cd1b61ce9914a80672ae1209f550
raviteja83 <rt12148@gmail.com>
2018-10-04 01:29:38 +0530

    Updates

---
+++ precache-manifest.18d4c46a2be3d8e1cb6735bf6b41a075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e77708616003e2cf066",
+    "url": "/SimpleDashboard/static/js/runtime~main.9e777086.js"
+  },
+  {
+    "revision": "ba7c35fe8e90f5935fcb",
+    "url": "/SimpleDashboard/static/js/main.ba7c35fe.chunk.js"
+  },
+  {
+    "revision": "e9e6caa097b1c14ca249",
+    "url": "/SimpleDashboard/static/js/1.e9e6caa0.chunk.js"
+  },
+  {
+    "revision": "ba7c35fe8e90f5935fcb",
+    "url": "/SimpleDashboard/static/css/main.5f49eec0.chunk.css"
+  },
+  {
+    "revision": "e9e6caa097b1c14ca249",
+    "url": "/SimpleDashboard/static/css/1.b8e89af2.chunk.css"
+  },
+  {
+    "revision": "da24a5372d07ecdf4b3ec88e24a29df7",
+    "url": "/SimpleDashboard/index.html"
+  }
+];
