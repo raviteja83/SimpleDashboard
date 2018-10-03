d77ac7be5f9d5022eb163b609bf3d5271a7f9822
raviteja83 <rt12148@gmail.com>
2018-10-04 01:38:09 +0530

    Updates

--- precache-manifest.4ad885440f265066e123f39c96e03ae9.js
+++ precache-manifest.4ad885440f265066e123f39c96e03ae9.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/SimpleDashboard/static/js/runtime~main.9e777086.js"
   },
   {
-    "revision": "c7e330a606510fd69e1f",
-    "url": "/SimpleDashboard/static/js/main.c7e330a6.chunk.js"
+    "revision": "2ef4ed92d7ab2f4908fa",
+    "url": "/SimpleDashboard/static/js/main.2ef4ed92.chunk.js"
   },
   {
     "revision": "e9e6caa097b1c14ca249",
     "url": "/SimpleDashboard/static/js/1.e9e6caa0.chunk.js"
   },
   {
-    "revision": "c7e330a606510fd69e1f",
+    "revision": "2ef4ed92d7ab2f4908fa",
     "url": "/SimpleDashboard/static/css/main.cfd14b89.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/SimpleDashboard/static/css/1.b8e89af2.chunk.css"
   },
   {
-    "revision": "df37570075a66aaa090875ea83a30981",
+    "revision": "e05272f980f18d411c4ecf4123a742ed",
     "url": "/SimpleDashboard/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/SimpleDashboard/precache-manifest.70b87ed8271f2bd37d36ab38f3a88a34.js"
+  "/SimpleDashboard/precache-manifest.4ad885440f265066e123f39c96e03ae9.js"
 );
 
 workbox.clientsClaim();
